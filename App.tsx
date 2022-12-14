14288136ea4a3333cc03090d29951ad90944e917
Oleh Bazyla <olegbazila@gmail.com>
2022-12-14 03:41:01 +0200

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>
+          some text
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
